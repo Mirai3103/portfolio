cda475810329b82576eea4891c486a2fc6f7143b
Mirai3103 <huuhoag1412@gmail.com>
2024-03-07 00:45:08 +0700

    Updates

--- _next/static/A03vmTdUpi3rlJGjwgBBC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/969-25e4ae8189c1d5b6.js","static/chunks/pages/index-43462909f720008b.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/NGX3wyljwI0P4OAvEEQlk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/969-25e4ae8189c1d5b6.js","static/chunks/pages/index-9892f87885598fa7.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
