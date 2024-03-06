07d5d8cb28c7f5e6e8f575386dccef6550341ea1
Mirai3103 <huuhoag1412@gmail.com>
2024-03-07 00:48:43 +0700

    Updates

---
+++ _next/static/0kEjdUnP_EphEQPssXL2p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/969-25e4ae8189c1d5b6.js","static/chunks/pages/index-6443dbae6875f42d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
